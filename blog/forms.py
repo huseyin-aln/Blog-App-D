79d3f0430d0b6888bf41a07c1082383f1fe3726d
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-09-12 10:55:36 +0300

    blog forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,21 @@
 from django import forms
-from .models import Post
+from .models import Category, Post, Comment
 
 class PostForm(forms.ModelForm):
+    status = forms.ChoiceField(choices=Post.OPTIONS)
+    category = forms.ModelChoiceField(queryset = Category.objects.all(), empty_label="Select")
     class Meta:
         model = Post
-        # fields = "__all__"
-        exclude = ('user',)
+        fields = (
+            'title',
+            'content',
+            'image',
+            'category',
+            'status'
+        )
+        
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('content',)
